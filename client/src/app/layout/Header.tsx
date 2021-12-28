2d3bda03c192e9ad0a505566f2b403c0f6422e45
acesko1 <acesko1@etf.unsa.ba>
2021-12-28 13:53:02 +0100

    End of section 9

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -3,6 +3,7 @@ import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography 
 import { Box } from "@mui/system";
 import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props{
     darkMode: boolean;
@@ -28,6 +29,7 @@ const navStyles = {
 }
 export default function Header({darkMode, handleThemeChange}: Props){
     const {basket} = useAppSelector(state => state.basket);
+    const {user} = useAppSelector(state => state.account)
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
     return (
         <AppBar position="static" sx={{mb:4}}>
@@ -53,15 +55,16 @@ export default function Header({darkMode, handleThemeChange}: Props){
                         <ShoppingCart />
                     </Badge>
                 </IconButton>
-              
-                
-                <List sx={{display: 'flex'}}>
+                {user ?(
+                    <SignedInMenu />) : (
+                        <List sx={{display: 'flex'}}>
                     {rightLinks.map(({title, path}) => (
                         <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
                             {title.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
+                    )}
                   </Box>
                 
             </Toolbar>
--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -2,12 +2,14 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { basketSlice } from "../../features/basket/basketSlice";
 import {configureStore} from "@reduxjs/toolkit";
 import { catalogSlice } from "../../features/catalog/catalogSlice";
+import { accountSlice } from "../../features/account/accountSlice";
 
 export const store = configureStore ({
 
         reducer: {
             basket: basketSlice.reducer,
-            catalog: catalogSlice.reducer
+            catalog: catalogSlice.reducer,
+            account: accountSlice.reducer
         }
 })
 export type RootState = ReturnType<typeof store.getState>;
